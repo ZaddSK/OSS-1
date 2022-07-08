320dee0405b549f864aae70b41aa52763f58f526
ShaikhShaibaz <108930298+ShaikhShaibaz@users.noreply.github.com>
2022-07-08 15:52:13 +0530

    Second Commit

---
+++ ArrayElement.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ArrayElement {
+
+	public static void main(String[] args) {
+		try
+		{
+			int arr[],size;
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter Array Size: ");
+			size= sc.nextInt();
+			arr=new int[size];
+			System.out.println("Enter Elements: ");
+			for(int i=0;i<size;i++)
+			{
+				arr[i]=sc.nextInt();
+			}
+			int index;
+			int element;
+			System.out.println("Enter the Index");
+			index=sc.nextInt();
+			element=arr[index];
+			System.out.println("Element at index: "+index+"="+element);
+			
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Wrong Index");
+		}
+		finally
+		{
+			System.out.println("completed");
+		}
+
+	}
+
+}
